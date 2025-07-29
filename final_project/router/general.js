d41e77b172c9e6cc95e3f065ddb59a340c25b8fd
Sadgunraj <sadgunraj6@gmail.com>
2025-07-28 23:41:15 -0400

    Updated files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const books = require("./booksdb.js");
 let public_users = express.Router();
+const axios = require("axios");
 
 public_users.get('/', (req, res) => {
   res.send(JSON.stringify(books, null, 4));
@@ -40,4 +41,52 @@ public_users.get('/review/:isbn', (req, res) => {
   book ? res.send(book.reviews) : res.status(404).send({ message: "Book not found" });
 });
 
+public_users.get('/books-async', async (req, res) => {
+  try {
+    const response = await axios.get('http://localhost:3000/');
+    res.status(200).json({
+      message: "Fetched books using async/await + Axios",
+      data: response.data
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch books using Axios",
+      error: error.message
+    });
+  }
+});
+
+public_users.get('/bookdetails-async/:isbn', async (req, res) => {
+    const isbn = req.params.isbn;
+    try {
+      const response = await axios.get(`http://localhost:3000/isbn/${isbn}`);
+      res.status(200).json({
+        message: `Book details for ISBN ${isbn} fetched using async/await`,
+        data: response.data
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to fetch book details",
+        error: error.message
+      });
+    }
+  });
+
+  public_users.get('/author-async/:author', async (req, res) => {
+    const author = req.params.author;
+    try {
+      const response = await axios.get(`http://localhost:3000/author/${author}`);
+      res.status(200).json({
+        message: `Books by '${author}' fetched using async/await`,
+        books: response.data
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to fetch books by author",
+        error: error.message
+      });
+    }
+  });
+
 module.exports.general = public_users;
+
